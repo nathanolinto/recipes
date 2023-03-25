aaaf47ba2a051a155654b00406e53975c84304b2
Nathan Olinto <nathanolinto@gmail.com>
2023-03-25 17:45:15 -0300

    chore: adiciona o testing-library

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,6 +1,13 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
 describe("jest", () => {
   it("should work", () => {
     expect(1).toBe(1);
+  });
+
+  it("should display elements", () => {
+    render(<App />);
   });
 });
 
